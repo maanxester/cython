d04ac1f1217928282a8fab543ea471b180a25f86
maanxester <maanxester21@hotmail.com>
2021-11-09 23:41:14 -0300

    Cython implementado

---
+++ fib_cy.py
@@ -0,0 +1,21 @@
+
+# Cython compilando
+
+# -a gera um HTML do código convertido e mostra as chamadas
+# -i cria um módulo que podemos importar no python
+# $ cythonize -a -i fib_py.py
+
+
+def fib(n):
+    a = 0
+    b = 1
+
+    # Não existe em C
+    for i in range(n):
+        a, b = a + b, a
+
+    return a
+
+
+# Retorna um arquivo C com muitas linhas de código, pois o C precisa entender a tipagem, erros etc.
+# Evitando NullPointer, Stack OverFlow.
--- run.py
+++ run.py
@@ -2,6 +2,10 @@
 from timeit import timeit
 
 py = timeit('fib(93)', number=1_000_000, setup='from fib_py import fib')
+cy = timeit('fib(93)', number=1_000_000, setup='from fib_cy import fib')
 
 print("Tempo do Python Puro: ", py)
 # 4.7648235
+
+print("Tempo do Cython: ", cy)
+print(f'{py/cy}')
